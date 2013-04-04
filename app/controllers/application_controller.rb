1096eb1168d453c001f87970d37dc7a696150323
VuongHo <vuongminho68@gmail.com>
2013-04-04 16:13:07 +0700

    moving the cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
-  def current_cart
-  	Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-  	cart = Cart.create
-  	session[:cart_id] = cart.id
-  	cart
-  end
+
+    def set_cart
+      @cart = Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	@cart = set_cart
   end
 end
