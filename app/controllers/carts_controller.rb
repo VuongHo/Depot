ec5a0ad7708c4e43e5da25202bbeff9e7fc1e36d
VuongHo <vuongminho68@gmail.com>
2013-04-04 14:40:25 +0700

    Finishing the Cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
   def index
@@ -74,10 +75,17 @@ class CartsController < ApplicationController
   def destroy
     @cart = Cart.find(params[:id])
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
+  end
+
+  private
+  def invalid_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to store_url, notice: 'Invalid cart'
   end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :product
+  attr_accessible :product, :product_id
   belongs_to :product
   belongs_to :cart
   # attr_accessible :title, :body
+  def total_price
+  	product.price * quantity
+  end
 end
