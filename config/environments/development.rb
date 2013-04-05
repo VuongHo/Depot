e30ac2edaa930a95e03ca9625fc57a677d2c96fc
VuongHo <vuongminho68@gmail.com>
2013-04-05 16:51:09 +0700

    Finish Cart and Creat User

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "vuonghominh9x@gmail.com",
+    password: "vuonghominhbkit4u",
+    enable_starttls_auto: true
+  }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404062559) do
+ActiveRecord::Schema.define(:version => 20130405092724) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -24,10 +24,20 @@ ActiveRecord::Schema.define(:version => 20130404062559) do
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
     t.integer  "quantity",   :default => 1
+    t.integer  "order_id"
   end
 
   add_index "line_items", ["cart_id"], :name => "index_line_items_on_cart_id"
   add_index "line_items", ["product_id"], :name => "index_line_items_on_product_id"
+
+  create_table "orders", :force => true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -36,6 +46,13 @@ ActiveRecord::Schema.define(:version => 20130404062559) do
     t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
